40d878dad402112f7169465c5be4aa650160aeb7
yudai-yam <yudaimadeinjapan@gmail.com>
2024-07-10 15:11:40 +0900

    blueprint

---
+++ main.py
@@ -0,0 +1,32 @@
+def scanner(file_path):
+    result = 0
+    
+    # read the file
+    with open(file_path, 'r') as file:
+
+
+    # look for a number
+
+    # get a full number
+
+    # get the index to be checked
+
+    # check if there is any adjacent symbol 
+
+    # if yes, sum up
+
+
+    return result
+
+
+
+def get_index()
+
+
+
+
+if __name__ == "__main__":
+
+    file_name = "test.txt"
+
+    result = scanner(file_name)
