afe86e4608018c2a28c2b0f0d90e5583c977e3f8
yudai-yam <yudaimadeinjapan@gmail.com>
2024-07-11 21:41:39 +0900

    done the finding number and its index part

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ def scanner(file_path):
     # read the file
     with open(file_path, 'r') as file:
 
-
-    # look for a number
-
+        lines = file.readlines()
+    
+   
     # get a full number
 
     # get the index to be checked
+
+    get_number_data(lines)
 
     # check if there is any adjacent symbol 
 
@@ -19,8 +21,48 @@ def scanner(file_path):
     return result
 
 
+# returns the number found and the index to be checked if there is a symbol
+def get_number_data(lines):
+    number_index_list = []
+    row = 0
+    column = 0
 
-def get_index()
+    # look for a number
+    for line in lines:
+        while(column<len(line)-1):
+            char = line[column]
+
+            # get a full number
+            if char.isnumeric():
+                number = ""
+                index = []
+                while(char.isnumeric()):
+                    number = number + char
+                    index.append([column, row])
+                    column = column + 1
+                    char = line[column]
+
+                number_index_list.append([number, index])
+                
+            print("a")
+            column = column + 1
+
+    
+    print(number_index_list)
+    
+        
+# get all the adjacent indices
+def get_adjacent_index(index):
+    movement_list = [[-1,0], [1,0], [-1,-1], [-1, 1], [0,-1], [0,1], [1,1], [1,-1]]
+    adjacent_indices = []
+    for movement in movement_list:
+        adjacent_indices.append(movement + index)
+
+    print(adjacent_indices)
+    return adjacent_indices
+
+
+
 
 
 
